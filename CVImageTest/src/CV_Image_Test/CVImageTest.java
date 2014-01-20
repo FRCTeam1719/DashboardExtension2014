d3f665e001ae7f84fd32cf2429b18a1eb4566b50
aaroneline <aaroneline@192.168.10.71>
2014-01-20 12:56:17 -0500

    -Finished contours

--- CVImageTest/src/CV_Image_Test/CVImageTest.java
+++ CVImageTest/src/CV_Image_Test/CVImageTest.java
@@ -1,21 +1,24 @@
 package CV_Image_Test;
 //TODO: Check for Memory Leaks
 //TODO: Check into hue issues
+//TODO: Comment out debug windows
+//TODO: Comments/end bracket notes
 import static CV_Image_Test.CVImageTest.win;
 import com.googlecode.javacv.CanvasFrame;
 import com.googlecode.javacv.cpp.opencv_core;
 import com.googlecode.javacv.cpp.opencv_core.*;
 import com.googlecode.javacv.cpp.opencv_imgproc;
 import com.googlecode.javacv.cpp.opencv_imgproc.*;
-import com.googlecode.javacv.cpp.opencv_objdetect;
 import edu.wpi.first.smartdashboard.camera.WPICameraExtension;
 import edu.wpi.first.wpijavacv.DaisyExtensions;
+import edu.wpi.first.wpijavacv.WPIBinaryImage;
 import edu.wpi.first.wpijavacv.WPIColor;
 import edu.wpi.first.wpijavacv.WPIColorImage;
 import edu.wpi.first.wpijavacv.WPIContour;
 import edu.wpi.first.wpijavacv.WPIImage;
 import edu.wpi.first.wpijavacv.WPIPoint;
 import edu.wpi.first.wpijavacv.WPIPolygon;
+import edu.wpi.first.wpilibj.networktables.NetworkTable;
 import java.io.File;
 import java.util.ArrayList;
 
@@ -78,12 +81,14 @@ public class CVImageTest extends WPICameraExtension {
     static WPIColorImage rawImage = null;
     static WPIImage resultImage = null;
     static boolean validImage;
+    //Grab Netowrk Table
+    public static NetworkTable SmartDashboard = NetworkTable.getTable("SmartDashboard");
 
     public CVImageTest() {
         super();
         //Create thresholdSlider
         win = new ThresholdSlider();
-
+        SmartDashboard.putBoolean("windows?", false);
         //Create canvas frames
         morph_result = new CanvasFrame("morph");
         morph_result.setLocation(900, 0);
@@ -114,7 +119,7 @@ public class CVImageTest extends WPICameraExtension {
         hue_mask2_win.setLocation(600, 300);
         hsv_frame = new CanvasFrame("HSV");
         hsv_frame.setLocation(400, 400);
-        
+
 
 
 
@@ -188,7 +193,7 @@ public class CVImageTest extends WPICameraExtension {
             linePt2 = new WPIPoint(size.width() / 2 + horizontalOffsetPixels, size.height() / 2 + 100);
             hLinePt3 = new WPIPoint(size.width() / 2 + horizontalOffsetPixels + 25, size.height() / 2 + 75);
             hLinePt4 = new WPIPoint(size.width() / 2 + horizontalOffsetPixels - 25, size.height() / 2 + 75);
-        }
+        } //if
 //        // Get the raw IplImages for OpenCV
         IplImage input = DaisyExtensions.getIplImage(rawImage);
         WPIColorImage output = new WPIColorImage(rawImage.getBufferedImage());
@@ -197,7 +202,7 @@ public class CVImageTest extends WPICameraExtension {
         opencv_core.cvSplit(hsv, hue, sat, val, null);
         hsv_frame.showImage(hsv.getBufferedImage());
         hue_win.showImage(hue.getBufferedImage());
-        
+
         //Uncomment the lines below to see intermediate images
 
 
@@ -207,7 +212,7 @@ public class CVImageTest extends WPICameraExtension {
         // a threshold and inverted threshold in order to get points that are in a narrow range
         // values above threshold are converted to white(255) and values below are converted to black(0)
         //red is 0 to maybe 45.  green 50-75 range
-        
+
         //Hue
         opencv_imgproc.cvThreshold(hue, hue_mask, ThresholdSlider.hueLowerSlider.getValue(), 255, opencv_imgproc.CV_THRESH_BINARY); //everything above here we want
 
@@ -229,84 +234,96 @@ public class CVImageTest extends WPICameraExtension {
         opencv_core.cvAnd(hue_mask, bin, bin, null);
         opencv_core.cvAnd(bin, sat_mask, bin, null);
         opencv_core.cvAnd(bin, val_mask, bin, null);
-        
+
 
         // Uncomment the line below to see resultant image after masking
 //        result.showImage(bin.getBufferedImage());
 
         //Show bin and hue before morphology is applied
-        bin_frame.showImage(bin.getBufferedImage());
-        hue_frame.showImage(hue_mask.getBufferedImage());
+        //bin_frame.showImage(bin.getBufferedImage());
+        //hue_frame.showImage(hue_mask.getBufferedImage());
 //        // Fill in any gaps using binary morphology
         opencv_imgproc.cvMorphologyEx(bin, bin, null, morphKernel, opencv_imgproc.CV_MOP_CLOSE, kHoleClosingIterations);
 //
 //        // Uncomment the next two lines to see the image post-morphology
 
-        morph_result.showImage(bin.getBufferedImage());
-        original.showImage(rawImage.getBufferedImage());
-        
-        //Display sat and val channals
-        sat_frame.showImage(sat_mask.getBufferedImage());
-        val_frame.showImage(val_mask.getBufferedImage());
-        opencv_objdetect.cvHaarDetectObjects(bin, 1, 0, 0, new CvSize(0,0), CvSize.ZERO);
+        //morph_result.showImage(bin.getBufferedImage());
+        //original.showImage(rawImage.getBufferedImage());
 
-//      
-//        // Find contours
-//        WPIBinaryImage binWpi = DaisyExtensions.makeWPIBinaryImage(bin);
-//        contours = DaisyExtensions.findConvexContours(binWpi);
-//
-//        polygons = new ArrayList<WPIPolygon>();
-//        // top target ratio = .32, mid target ratio = .47
-//        for (WPIContour c : contours) {
-//            double ratio = ((double) c.getHeight()) / ((double) c.getWidth());
-//
-//            //uncomment the following 2 lines to see scoring results
-//            rawImage.drawContour(c, WPIColor.WHITE, 1);
-//            System.out.println("ratio = " + ratio + " width = " + c.getWidth());
-//
-//            if (ratio < .6 && ratio > 0.2 && c.getWidth() > kMinWidth && c.getWidth() < kMaxWidth) {
-//                polygons.add(c.approxPolygon(20));
-//                rawImage.drawContour(c, WPIColor.BLUE, 2);
-//            }
-//        }
-//
-////Find highest polygon   
-//        WPIPolygon rectangle = null;
-//        int highest = Integer.MAX_VALUE;
-//
-//        for (WPIPolygon p : polygons) {
-//            int pCenterX = (p.getX() + (p.getWidth() / 2));
-//            int pCenterY = (p.getY() + (p.getHeight() / 2));
-//
-//            if (pCenterY < highest) // Because coord system is funny
-//            {
-//                rectangle = p;
-//                highest = pCenterY;
-//                rawImage.drawPoint(new WPIPoint(pCenterX, pCenterY), targetColor, 3);
-//            }
-//        }
-//
-//        if (rectangle != null) {
-//            double x = rectangle.getX() + (rectangle.getWidth() / 2);   // x value of center point of rectangle
-//            x = 2 * x / size.width() - 1;                           // convert to fraction of screen width off center
-//            double y = rectangle.getY() + (rectangle.getHeight() / 2);  // y value of center point of rectangle
-//            y = -(2 * y / size.height() - 1);                        // convert to fraction of screen height off center
-//
-//            double azimuth = x * kHorizontalFOVDeg / 2.0 - kShooterOffsetDeg;
-//            double elevation = y * kVerticalFOVDeg / 2.0 - kCameraPitchDeg;
-//            //TODO fix range calculation
-//            double range = (kTopTargetHeightIn - kCameraHeightIn) / Math.tan((y * kVerticalFOVDeg / 2.0 + kCameraPitchDeg) * Math.PI / 180.0);
-//
-//            System.out.println("Target found");
-//            System.out.println("x: " + x);
-//            System.out.println("y: " + elevation);
-//            System.out.println("azimuth: " + azimuth);
-//            System.out.println("range: " + range);
-//        }
-//
-//        // Draw a crosshair at screen center
-        output.drawLine(linePt1, linePt2, targetColor, 2);
-        output.drawLine(hLinePt3, hLinePt4, targetColor, 2);
+        //Display sat and val channals
+        //sat_frame.showImage(sat_mask.getBufferedImage());
+        //val_frame.showImage(val_mask.getBufferedImage());
+        
+        //Find Contours
+        WPIBinaryImage binWpi = DaisyExtensions.makeWPIBinaryImage(bin);
+        contours = DaisyExtensions.findConvexContours(binWpi);
+
+        polygons = new ArrayList<WPIPolygon>();
+
+        for (WPIContour c : contours) {
+            double ratio = ((double) c.getHeight()) / ((double) c.getWidth());
+
+            rawImage.drawContour(c, WPIColor.WHITE, 1);
+            System.out.println("ratio = " + ratio + " width = " + c.getWidth());
+
+            if (ratio < .6 && ratio > 0.2 && c.getWidth() > kMinWidth && c.getWidth() < kMaxWidth) {
+                polygons.add(c.approxPolygon(20));
+                rawImage.drawContour(c, WPIColor.BLUE, 2);
+            }//if
+
+        }//for
+
+        //Find Highest polygon
+        WPIPolygon rectangle = null;
+        int highest = Integer.MAX_VALUE;
+
+        for (WPIPolygon p : polygons) {
+            int pCenterX = (p.getX() + (p.getWidth() / 2));
+            int pCenterY = (p.getY() + (p.getHeight() / 2));
+
+            if (pCenterY < highest) {
+                rectangle = p;
+                highest = pCenterY;
+                rawImage.drawPoint(new WPIPoint(pCenterX, pCenterY), targetColor, 3);
+            }//if
+        } //for
+
+        if (rectangle != null) {
+            double x = rectangle.getX() + (rectangle.getWidth() / 2);   // x value of center point of rectangle
+            x = 2 * x / size.width() - 1;                           // convert to fraction of screen width off center
+            double y = rectangle.getY() + (rectangle.getHeight() / 2);  // y value of center point of rectangle
+            y = -(2 * y / size.height() - 1);                        // convert to fraction of screen height off center
+
+            double azimuth = x * kHorizontalFOVDeg / 2.0 - kShooterOffsetDeg;
+            double elevation = y * kVerticalFOVDeg / 2.0 - kCameraPitchDeg;
+            //TODO fix range calculation
+            double range = (kTopTargetHeightIn - kCameraHeightIn) / Math.tan((y * kVerticalFOVDeg / 2.0 + kCameraPitchDeg) * Math.PI / 180.0);
+
+            SmartDashboard.putBoolean("found", true);
+            SmartDashboard.putNumber("Az", azimuth);
+            SmartDashboard.putNumber("El", elevation);
+
+            System.out.println("Target found");
+            System.out.println("x: " + x);
+            System.out.println("y: " + elevation);
+            System.out.println("azimuth: " + azimuth);
+            System.out.println("range: " + range);
+
+        } else {
+            SmartDashboard.putBoolean("found", false);
+            SmartDashboard.putNumber("Az", 0);
+            SmartDashboard.putNumber("El", 0);
+            System.out.println("Target not found");
+        } //else
+
+
+
+
+
+
+        // Draw a crosshair at screen center
+        //output.drawLine(linePt1, linePt2, targetColor, 2);
+        //output.drawLine(hLinePt3, hLinePt4, targetColor, 2);
 //
         DaisyExtensions.releaseMemory();
 //
